b38088c8ba3f25897ae1a90257ada73713a936a4
SamuelVBF <samuel.bjj@hotmail.com>
2023-08-22 13:58:51 -0300

    Initial commit

---
+++ lib/screens/categories_screen.dart
@@ -0,0 +1,29 @@
+import 'package:flutter/material.dart';
+
+import 'package:multi_telas/components/category_item.dart';
+import '/data/dummy_data.dart';
+
+class CategoriesScreen extends StatelessWidget {
+  const CategoriesScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Vamos Cozinhar?'),
+      ),
+      body: GridView(
+        padding: EdgeInsets.all(25),
+        gridDelegate: SliverGridDelegateWithMaxCrossAxisExtent(
+          maxCrossAxisExtent: 200,
+          childAspectRatio: 3 / 2,
+          crossAxisSpacing: 20,
+          mainAxisSpacing: 20,
+        ),
+        children: DUMMY_CATEGORIES.map((cat) {
+          return CategoryItem(cat);
+        }).toList(),
+      ),
+    );
+  }
+}
